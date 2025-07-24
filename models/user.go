09b38b5ad9c4655245664ed6bc6204d176b5d70b
EfoJensen <Jensena231@gmail.com>
2025-07-24 19:39:25 +0000

    Cleaned up repeated code

--- models/user.go
+++ models/user.go
@@ -8,34 +8,31 @@ type SameFields struct {
 	Email       string    `json:"email" bson:"email"`
 	BirthDate   time.Time `json:"birthDate" bson:"birthDate"`
 	PhoneNumber string    `json:"phoneNumber" bson:"phoneNumber"`
+	Role        string    `json:"role" bson:"role"`
 }
 
 type Student struct {
 	SameFields
 	Hostel  string `json:"hostel" bson:"hostel"`
-	Role    string `json:"role" bson:"role"`
 	College string `json:"college" bson:"college"`
 	RefId   string `json:"refId" bson:"refId"`
 }
 
 type TeachAsst struct {
-    SameFields
-    Department string `json:"department" bson:"department"`
-    Role       string `json:"role" bson:"role"`
-    College    string `json:"college" bson:"college"`
-    StaffId    string `json:"staffId" bson:"staffId"`
+	SameFields
+	Department string `json:"department" bson:"department"`
+	College    string `json:"college" bson:"college"`
+	StaffId    string `json:"staffId" bson:"staffId"`
 }
 
 type Lecturer struct {
-    SameFields
-    Department string `json:"department" bson:"department"`
-    Role       string `json:"role" bson:"role"`
-    College    string `json:"college" bson:"college"`
-    StaffId    string `json:"staffId" bson:"staffId"`
+	SameFields
+	Department string `json:"department" bson:"department"`
+	College    string `json:"college" bson:"college"`
+	StaffId    string `json:"staffId" bson:"staffId"`
 }
 
 type Other struct {
-    SameFields
-    Role    string `json:"role" bson:"role"`
-    StaffId string `json:"staffId" bson:"staffId"`
+	SameFields
+	StaffId string `json:"staffId" bson:"staffId"`
 }
