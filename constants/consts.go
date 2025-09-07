8881905559225e5073e4b96a254fe0900e64ce64
EfoJensen <Jensena231@gmail.com>
2025-09-07 19:47:07 +0000

    Switched to public url

--- constants/consts.go
+++ constants/consts.go
@@ -1,6 +1,5 @@
 package constants
 
 const (
-	PubUrl = "http://localhost:4000/chat/community/68bc5cbbcd8d68664a8220fa/join?userId=";
-	PublicUrl = "https://rapport-backend.onrender.com/chat/community/68bc5cbbcd8d68664a8220fa/join?userId=";
+	PubUrl = "https://rapport-backend.onrender.com/chat/community/68bc5cbbcd8d68664a8220fa/join?userId=";
 )
