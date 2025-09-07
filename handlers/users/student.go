55abcf7f9979d55eb15f73009918a03540593e9c
EfoJensen <Jensena231@gmail.com>
2025-09-07 19:23:23 +0000

    Successful feature of user being automatically added to community on signUp

--- handlers/users/student.go
+++ handlers/users/student.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Efojensen/rapport.git/constants"
 	"github.com/Efojensen/rapport.git/handlers/secure"
 	"github.com/Efojensen/rapport.git/models"
 	"github.com/gofiber/fiber/v2"
@@ -35,8 +39,40 @@ func StudentProfileSetup(c *fiber.Ctx, collection *mongo.Collection) error {
 		student.ID = oid
 	}
 
+	res := fmt.Sprint(student.ID)
+
+	communityUrl := fmt.Sprint(constants.PubUrl, res[10:34])
+
+	proxy := fiber.AcquireAgent()
+	proxy.Request().Header.SetMethod("POST")
+	proxy.Request().SetRequestURI(communityUrl)
+
+	err = proxy.Parse()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"err": err.Error(),
+		})
+	}
+
+	var resBody struct {
+		Message string `json:"message"`
+	}
+
+	_, body, errs := proxy.Bytes()
+	if len(errs) > 0 {
+		return c.Status(fiber.StatusInternalServerError).JSON(errs)
+	}
+
+	err = json.Unmarshal(body, &resBody)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"err": err.Error(),
+		})
+	}
+
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"msg":     "student received successfully",
-		"student": student,
+		"msg":          "student received successfully",
+		"student":      student,
+		"communityMsg": resBody.Message,
 	})
 }
